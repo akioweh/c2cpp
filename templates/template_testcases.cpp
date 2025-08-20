1e2a834f3a25cc5faf4623e9da6897489f4fa756
akioweh <0@akioweh.com>
2025-08-20 19:27:19 +0400

    make the checks actually check

--- templates/template_testcases.cpp
+++ templates/template_testcases.cpp
@@ -28,17 +28,19 @@ vector<T> reads() {
 }
 
 
-template<size_t N, typename T = int, bool check_count = true>
+template<size_t N, typename T = int, bool validate = true>
 array<T, N> reads() {
     array<T, N> arr;
     istringstream iss(read());
-    auto r = ranges::copy(views::istream<T>(iss) | views::take(N), arr.begin());
-    if (check_count) {
+    auto issv = views::istream<T>(iss) | views::take(N);
+    const auto r = ranges::copy(issv, arr.begin());
+    if (validate) {
+        if (iss.fail() && !iss.eof())
+            throw runtime_error("reads() fail: parse error");
+        if (r.in.base() != default_sentinel_t{})
+            throw runtime_error("reads() fail: excess elements");
         if (r.out != arr.end())
-            throw runtime_error("not enough elements");
-        T _;
-        if (iss >> _ || !iss.eof())
-            throw runtime_error("too many elements");
+            throw runtime_error("reads() fail: not enough elements");
     }
     return arr;
 }
