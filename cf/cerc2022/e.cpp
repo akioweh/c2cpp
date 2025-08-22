b00104747f1afc74e3cde743ffc84d1488c0112f
akioweh <0@akioweh.com>
2025-08-22 13:33:06 +0400

    completed sol

--- cf/cerc2022/e.cpp
+++ cf/cerc2022/e.cpp
@@ -1,12 +1,13 @@
 #pragma GCC optimize("Ofast")
 
-#include <vector>
-#pragma GCC target("avx2,sse4.2,fma,bmi2,popcnt")
 #include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <numeric>
 #include <ranges>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -28,17 +29,19 @@ vector<T> reads() {
 }
 
 
-template<size_t N, typename T = int, bool check_count = true>
+template<size_t N, typename T = int, bool validate = true>
 array<T, N> reads() {
     array<T, N> arr;
     istringstream iss(read());
-    auto r = ranges::copy(views::istream<T>(iss) | views::take(N), arr.begin());
-    if (check_count) {
+    auto issv = views::istream<T>(iss) | views::take(N);
+    const auto r = ranges::copy(issv, arr.begin());
+    if (validate) {
+        if (iss.fail() && !iss.eof())
+            throw runtime_error("reads() fail: parse error");
+        if (r.in.base() != default_sentinel_t{})
+            throw runtime_error("reads() fail: excess elements");
         if (r.out != arr.end())
-            throw runtime_error("not enough elements");
-        T _;
-        if (iss >> _ || !iss.eof())
-            throw runtime_error("too many elements");
+            throw runtime_error("reads() fail: not enough elements");
     }
     return arr;
 }
@@ -46,11 +49,30 @@ array<T, N> reads() {
 
 void solve() {
     const auto [N] = reads<1>();
-    auto arr = reads<double>();
-    // auto inv = vector<double>(N);
-    // for (auto [v, iv] : views::zip(arr, inv))
-    //     iv = v / 1;
-    auto min_ = *ranges::min_element(arr);
+    auto arr = vector<double>(N);
+    for (auto &a : arr)
+        cin >> a;
+    cin.ignore();
+    const auto max_a = *ranges::max_element(arr);
+    ranges::transform(arr, arr.begin(), [&](const double v) { return v / max_a; });
+
+    auto best_error = 1e5;
+    auto best_mul = -1;
+    for (const auto max_x : views::iota(1, 10001) | views::reverse) {
+        const auto rn = arr | views::transform([&](const auto a) {
+                            const auto x = a * max_x;
+                            const auto xi = round(x);
+                            return abs(xi - x);
+                        });
+        const auto err = accumulate(rn.begin(), rn.end(), 0.);
+        if (err - 1e-7 < best_error) {
+            best_error = err;
+            best_mul = max_x;
+        }
+    }
+
+    for (const auto a : arr)
+        cout << static_cast<int>(round(a * best_mul)) << '\n';
 }
 
 
