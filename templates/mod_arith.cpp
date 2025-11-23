81d950041d706a77fa91e1253ff63e610614cc74
akioweh <0@akioweh.com>
2025-11-23 22:28:38 +0000

    temp

--- templates/mod_arith.cpp
+++ templates/mod_arith.cpp
@@ -6,48 +6,47 @@ using namespace std;
 using ll = long long;
 
 
-template<auto MOD_>
-    requires(integral<decltype(MOD_)> && MOD_ > 0)
+template<int MOD>
+    requires(MOD > 0)
 struct Mint {
-    using T = decltype(MOD_);
-    static constexpr T MOD = MOD_;
+    // invariant: v always fits in an int (as MOD fits in int)
+    ll v;
 
-    int v;
-
-    constexpr Mint() noexcept = default;  // v = 0
+    constexpr Mint() noexcept = default; // v = 0
     constexpr Mint(const ll x) noexcept : v(norm(x)) {}
 
 
     static constexpr int norm(ll x) noexcept {
         x %= MOD;
-        if (x < 0) x += MOD;
+        if (x < 0)
+            x += MOD;
         return static_cast<int>(x);
     }
 
 
     constexpr Mint &operator+=(const Mint rhs) noexcept {
         v += rhs.v;
-        if (v >= MOD) v -= MOD;
+        if (v >= MOD)
+            v -= MOD;
         return *this;
     }
 
 
     constexpr Mint &operator-=(const Mint rhs) noexcept {
         v -= rhs.v;
-        if (v < 0) v += MOD;
+        if (v < 0)
+            v += MOD;
         return *this;
     }
 
 
     constexpr Mint &operator*=(const Mint rhs) noexcept {
-        v = static_cast<int>(static_cast<ll>(v) * rhs.v % MOD);
+        v = static_cast<int>(v * rhs.v % MOD);
         return *this;
     }
 
 
-    constexpr Mint &operator/=(const Mint rhs) noexcept {
-        return *this *= rhs.inv();
-    }
+    constexpr Mint &operator/=(const Mint rhs) noexcept { return *this *= inv(rhs); }
 
 
     constexpr Mint operator-() const noexcept { return Mint(MOD - v); }
@@ -70,15 +69,17 @@ struct Mint {
 
 
     friend constexpr Mint pow(Mint base, ll e) noexcept {
-        if (e == -1) return inv(base);
+        if (e == -1)
+            return inv(base);
         assert(e >= 0);
-        Mint result(1);
+        auto res = Mint(1);
         while (e > 0) {
-            if (e % 2) result *= base;
+            if (e % 2)
+                res *= base;
             base *= base;
             e /= 2;
         }
-        return result;
+        return res;
     }
 
 
@@ -113,18 +114,13 @@ istream &operator>>(istream &is, Mint<M> &x) {
 }
 
 
-// CTAD (Class Template Argument Deduction) guide
-template<int M>
-Mint(int) -> Mint<M>;
-
-
 // usage example
 int main() {
     constexpr int P = 1'000'000'007;
-    Mint<P> a = 2;  // uses Mint(int) CTAD
+    Mint<P> a = 2;
     Mint<P> b = 5LL;
     auto c = 3 * a * b + 42;
-    cout << "c = " << c << "\n";       // c = 52
-    cout << "inv(c) = " << c.inv() << "\n";
+    cout << "c = " << c << "\n"; // c = 52
+    cout << "inv(c) = " << inv(c) << "\n";
     cout << "c^100 = " << pow(c, 100) << "\n";
 }
