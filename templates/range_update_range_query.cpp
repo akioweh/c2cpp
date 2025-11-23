cc475a182e148cbc297e563aaa6da1536f1eacbd
akioweh <0@akioweh.com>
2025-11-23 19:11:45 +0000

    finally fix up segtree??

--- templates/range_update_range_query.cpp
+++ templates/range_update_range_query.cpp
@@ -1,8 +1,11 @@
 #include <algorithm>
+#include <array>
+#include <bit>
 #include <cassert>
 #include <concepts>
 #include <functional>
 #include <iterator>
+#include <limits>
 #include <ranges>
 #include <type_traits>
 #include <vector>
@@ -17,22 +20,21 @@ template<typename T, typename F>
 concept upd_func = is_invocable_r_v<void, F, T &, T, size_t>;
 
 
-template<typename... Ts>
-auto nsum(const Ts &...a) {
-    return (a + ...);
-}
-
-
-template<typename T,
-         size_t N,
-         T Default = 0,
-         binary_func<T, T, T> auto Func = [](T a, T b) { return max(a, b); }, // default: max
-         T CacheDefault = 0,
-         upd_func<T> auto Upd = [](T &a, T b, size_t /*len*/) { a += b; } // default: addition  (&T, T, int) -> void
-         >
+template<size_t N,
+         typename T = long long,
+         T Default = numeric_limits<T>::min(), // identity for query (e.g., 0 for sum, -INF for max)
+         binary_func<T, T, T> auto Func = [](T a, T b) { return max(a, b); }, // fold function (default: max)
+         T CacheDefault = T{}, // identity for lazy (default: 0)
+         upd_func<T> auto Upd = [](T &a, T b, size_t /*len*/) { a += b; }, // update function (default: add)
+         size_t H = bit_width(N)>
 struct RURQ {
     array<T, 2 * N> data;
     array<T, N> cache;
+
+    RURQ() {
+        ranges::fill(data, Default);
+        ranges::fill(cache, CacheDefault);
+    }
 
     // construct from range/iterator
     template<ranges::input_range R>
@@ -45,37 +47,11 @@ struct RURQ {
             data[i] = Func(data[2 * i], data[2 * i + 1]);
     }
 
-    // pushes (internal) node i's updates to its direct children
-    void push1(const size_t i) {
-        if (!cache[i])
-            return;
-        cache[i] = CacheDefault;
-        Upd(data[2 * i], cache[i], 1);
-        Upd(data[2 * i + 1], cache[i], 1);
-        if (2 * i < N) {
-            cache[2 * i] += cache[i];
-            cache[2 * i + 1] += cache[i];
-        }
-    }
-
-    // propagates all updates on path from root to node i
-    void push(const size_t i) {
-        for (auto s = bit_width(i); s > 0; s--)
-            push1(i >> s);
-    }
-
-    // full propagation (push all lazy to leaves)
-    void propagate() {
-        for (auto i = 1; i < N; i++)
-            push(i);
-    }
-
-    // recomputes internal nodes on path from i to root
-    void pull(size_t i) {
-        while (i /= 2) {
-            data[i] = Func(data[2 * i], data[2 * i + 1]);
-            Upd(data[i], cache[i], 1);
-        }
+    static size_t node_size(const size_t _i) {
+        if (_i >= N)
+            return 1ull;
+        const auto s = H - bit_width(_i);
+        return 1ull << (s + (_i < (N >> s)));
     }
 
     // collects the exact nodes representing the range [l, r), in order
@@ -89,10 +65,38 @@ struct RURQ {
         }
         if (reverse)
             swap(nodes_l, nodes_r);
-        // nodes_l.insert(nodes_l.end(), nodes_r.rbegin(), nodes_r.rend());
         nodes_l.reserve(nodes_l.size() + nodes_r.size());
         ranges::copy(nodes_r | views::reverse, back_inserter(nodes_l));
         return nodes_l;
+    }
+
+    // pushes (internal) i's updates to its direct children
+    void push1(const size_t _i, const size_t len) {
+        if (cache[_i] == CacheDefault)
+            return;
+
+        Upd(data[2 * _i], cache[_i], len >> 1);
+        Upd(data[2 * _i + 1], cache[_i], len >> 1);
+        if (2 * _i < N) {
+            cache[2 * _i] += cache[_i];
+            cache[2 * _i + 1] += cache[_i];
+        }
+        cache[_i] = CacheDefault;
+    }
+
+    // propagates all updates on path from root to i
+    void push(const size_t _i) {
+        for (auto s = bit_width(_i); s; --s)
+            push1(_i >> s, 1 << s);
+    }
+
+    // recomputes nodes on path from i to root
+    void pull(size_t _i) {
+        size_t len = node_size(_i);
+        while (_i >>= 1, len <<= 1) {
+            data[_i] = Func(data[2 * _i], data[2 * _i + 1]);
+            Upd(data[_i], cache[_i], len);
+        }
     }
 
     void set(const size_t i, const T val) {
@@ -102,8 +106,10 @@ struct RURQ {
     }
 
     void update(const size_t l, const size_t r, T val) {
-        for (auto i : collect(l, r)) {
-            Upd(data[i], val, 1);
+        push(l + N); // necessary if Upd is not commutative
+        push(r + N - 1);
+        for (const auto i : collect(l, r)) {
+            Upd(data[i], val, node_size(i));
             if (i < N)
                 cache[i] += val;
         }
@@ -118,31 +124,26 @@ struct RURQ {
                                  Func);
     }
 
-    size_t bound(
-            const T value, const size_t l, const size_t r, const binary_func<T, T, bool> auto cmp, const bool reverse) {
+    // generalized binary search; finds left- or right-most element satisfying pred
+    // pred should return true on node's value if target is within node's subtree
+    size_t find(const size_t l, const size_t r, const function<bool(T)> pred, const bool reverse) {
         push(l + N);
         push(r + N - 1);
+
         for (auto i : collect(l, r, reverse)) {
-            if (cmp(data[i], value)) {
-                while (i < N) {
-                    push1(i);
-                    auto nxt = 2 * i + reverse;
-                    if (cmp(data[nxt], value))
-                        i = nxt;
-                    else
-                        i = nxt ^ 1;
-                }
-                return i - N;
+            if (!pred(data[i]))
+                continue;
+            while (i < N) {
+                push1(i, node_size(i));
+                const auto nxt = (i << 1) + reverse;
+                i = pred(data[nxt]) ? nxt : nxt ^ 1;
             }
+            return i - N;
         }
-        return reverse ? l : r;
+        return r; // not found
     }
 
     size_t lower_bound(const T value, const size_t l = 0, const size_t r = N) {
-        return bound(value, l, r, less{}, false);
-    }
-
-    size_t upper_bound(const T value, const size_t l = 0, const size_t r = N) {
-        return bound(value, l, r, less_equal{}, false);
+        return find(l, r, [&](const T &v) { return v >= value; }, false);
     }
 };
